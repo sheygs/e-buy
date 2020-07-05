5303125b21ab4a30e53dbe0364e02e2032e05cd8
sheygs13 <segun.ekoh@gmail.com>
2020-07-05 14:16:54 +0100

    chore(context-api): set-up App-level state management

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import "font-awesome/css/font-awesome.css";
+import { ProductProvider } from "./services/context";
 import App from "./App";
 
 ReactDOM.render(
   <StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <ProductProvider>
+      <Router>
+        <App />
+      </Router>
+    </ProductProvider>
   </StrictMode>,
   document.getElementById("root")
 );
