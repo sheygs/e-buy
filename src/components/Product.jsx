85dab0c0e38095d630b8ef81ab045c1196225ba4
sheygs13 <segun.ekoh@gmail.com>
2020-07-05 22:11:14 +0100

    feat(Product): style product item

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -12,9 +12,12 @@ export default class Product extends Component {
     return (
       <ProductContainer className="col-9 mx-auto col-md-6 col-lg-3 my-3">
         <div className="card">
-          <div className="img-container p-5" onClick={() => console.log(title)}>
+          <div
+            className="image-container p-5"
+            onClick={() => console.log(title)}
+          >
             <Link to="/details">
-              <img src={img} alt={title} className="card-img-top" />
+              <img src={img} alt={title} className="card-image-top" />
             </Link>
             <button
               className="cart-btn"
@@ -28,7 +31,7 @@ export default class Product extends Component {
               )}
             </button>
           </div>
-          <div className="d-flex justify-content-between mx-2 border-top py-2">
+          <div className="card-info d-flex justify-content-between mx-2 border-top py-2">
             <p className="text-title align-self-center mb-0">
               {truncate(title)}
             </p>
@@ -42,4 +45,63 @@ export default class Product extends Component {
   }
 }
 
-const ProductContainer = styled.div``;
+const ProductContainer = styled.div`
+  .card {
+    border-color: transparent;
+    transition: all 1.5s linear;
+  }
+
+  .card-info {
+    border-top: transparent;
+    background: transparent;
+    transition: all 1.5s linear;
+  }
+
+  &:hover {
+    .card {
+      border: 0.75px solid rgba(0, 0, 0, 0.25);
+      box-shadow: 3px 3px 6px 0px rgba(0, 0, 0, 0.2);
+    }
+
+    .card-info {
+      background: rgba(246, 245, 247);
+    }
+  }
+
+  .image-container {
+    position: relative;
+    overflow: hidden;
+  }
+
+  .card-image-top {
+    display: block;
+    max-width: 100%;
+    transition: all 1s linear;
+  }
+
+  .image-container:hover .card-image-top {
+    transform: scale(1.15);
+  }
+
+  .cart-btn {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    padding: 0.2em 0.5em;
+    border: none;
+    background: var(--secondaryBlue);
+    cursor: pointer;
+    color: var(--nearWhite);
+    border-radius: 5px 0 0 0;
+    transform: translate(100%, 100%);
+    transition: all 1s linear;
+  }
+
+  .cart-btn:hover {
+    color: var(--bgDark);
+  }
+
+  .image-container:hover .cart-btn {
+    transform: translate(0, 0);
+  }
+`;
