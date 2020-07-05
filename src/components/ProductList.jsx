092d9d77e6b224fb4c7988f9ed54353a7f8ec68c
sheygs13 <segun.ekoh@gmail.com>
2020-07-05 14:13:48 +0100

    chore(list view): set up product list views

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
 import Product from "./Product";
+import Title from "../common/Title";
+import { ProductConsumer } from "../services/context";
 
 export default class ProductList extends Component {
   render() {
     return (
-      <div>
-        <Product />
+      <div className="py-3">
+        <div className="container">
+          <Title title="All Products" />
+          <div className="row">
+            {
+              <ProductConsumer>
+                {({ products }) =>
+                  products.map((product, i) => (
+                    <Product key={i} product={product} />
+                  ))
+                }
+              </ProductConsumer>
+            }
+          </div>
+        </div>
       </div>
     );
   }
