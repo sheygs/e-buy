f1f0e5044cf87ad8e8ceb0248afbc86f78c6642a
sheygs13 <segun.ekoh@gmail.com>
2020-07-06 09:27:56 +0100

    fix(product prop):fix  prop-type validation error

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,49 +1,44 @@
-import React, { Component } from "react";
+import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import { truncate, formatCurrency } from "../utils/helpers";
+import { truncate, formatPrice } from "../utils/helpers";
+import PropTypes from "prop-types";
 import { ProductConsumer } from "../services/context";
 
-export default class Product extends Component {
-  render() {
-    const {
-      product: { id, title, img, price, inCart },
-    } = this.props;
-    return (
-      <ProductContainer className="col-9 mx-auto col-md-6 col-lg-3 my-3">
-        <div className="card">
-          <div
-            className="image-container p-5"
-            onClick={() => console.log(title)}
+const Product = ({ product: { id, title, img, price, inCart } }) => {
+  return (
+    <ProductContainer className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+      <div className="card">
+        <div
+          className="image-container p-5"
+          onClick={() => console.log(id, title)}
+        >
+          <Link to="/details">
+            <img src={img} alt={title} className="card-image-top" />
+          </Link>
+          <button
+            className="cart-btn"
+            disabled={inCart}
+            onClick={() => console.log("added to cart")}
           >
-            <Link to="/details">
-              <img src={img} alt={title} className="card-image-top" />
-            </Link>
-            <button
-              className="cart-btn"
-              disabled={inCart}
-              onClick={() => console.log("added to cart")}
-            >
-              {inCart ? (
-                <span className="text-capitalize mb-0">in cart</span>
-              ) : (
-                <i className="fa fa-shopping-cart" aria-hidden="true" />
-              )}
-            </button>
-          </div>
-          <div className="card-info d-flex justify-content-between mx-2 border-top py-2">
-            <p className="text-title align-self-center mb-0">
-              {truncate(title)}
-            </p>
-            <span className="text-price align-self-center mb-0">
-              {formatCurrency(price, "NGN")}
-            </span>
-          </div>
+            {inCart ? (
+              <span className="text-capitalize mb-0">in cart</span>
+            ) : (
+              <i className="fa fa-shopping-cart" aria-hidden="true" />
+            )}
+          </button>
         </div>
-      </ProductContainer>
-    );
-  }
-}
+        <div className="card-info d-flex justify-content-between mx-2 border-top py-2">
+          <p className="text-title align-self-center mb-0">{truncate(title)}</p>
+          <span className="text-price align-self-center mb-0">
+            &#x20A6;
+            {formatPrice(price)}
+          </span>
+        </div>
+      </div>
+    </ProductContainer>
+  );
+};
 
 const ProductContainer = styled.div`
   .card {
@@ -105,3 +100,15 @@ const ProductContainer = styled.div`
     transform: translate(0, 0);
   }
 `;
+
+Product.propTypes = {
+  product: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    inCart: PropTypes.bool.isRequired,
+  }).isRequired,
+};
+
+export default Product;
