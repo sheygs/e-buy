9fd4d5cf35ae6ebf23ab0d0db6d91b22af958291
sheygs13 <segun.ekoh@gmail.com>
2020-07-06 08:45:15 +0100

    chore(product price): format product price

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,18 +1,10 @@
-function formatCurrency(amount, currency) {
-  return removeDecimalSpaces(
-    amount.toLocaleString("en-US", {
-      style: "currency",
-      currency,
-    })
-  );
-}
-
-function removeDecimalSpaces(string) {
-  return string.split(".")[0];
+function formatPrice(price) {
+  // adds comma(s) to number
+  return `${price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`;
 }
 
 const truncate = (words, limit = 15) => {
   return words.length > limit ? `${words.slice(0, limit)}...` : words;
 };
 
-export { truncate, formatCurrency };
+export { truncate, formatPrice };
