2cdebbe70efb2d543b5014c6a8420984dc584aea
sheygs13 <segun.ekoh@gmail.com>
2020-07-08 14:07:57 +0100

    chore: cart column headers

---
+++ src/components/CartColumns.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+export default class CartColumns extends Component {
+  columns = [
+    { id: 1, title: "products" },
+    { id: 2, title: "name" },
+    { id: 3, title: "price" },
+    { id: 4, title: "quantity" },
+    { id: 5, title: "remove" },
+    { id: 6, title: "total" },
+  ];
+  render() {
+    const { columns } = this;
+    return (
+      <div className="container-fluid text-center d-none d-lg-block">
+        <div className="row">
+          {columns.map((column) => (
+            <div key={column.id} className="col-10 mx-auto col-lg-2">
+              <p className="text-uppercase">{column.title}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
