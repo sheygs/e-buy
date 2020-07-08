d62ad5ff6a3fce4e50c9d37a1f327ba8fb95de43
sheygs13 <segun.ekoh@gmail.com>
2020-07-08 14:09:48 +0100

    feat(empty cart): implement empty cart method

---
+++ src/components/EmptyCart.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Title from "../common/Title";
+
+const EmptyCart = () => {
+  return <Title title="There are no items in your cart." />;
+};
+
+export default EmptyCart;
