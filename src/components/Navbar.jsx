3e787af490c92224531065e5010b38278dd5f76f
sheygs13 <segun.ekoh@gmail.com>
2020-07-10 23:56:54 +0100

    feat(cart-logic): implement shopping cart count

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Button from "../common/Button";
 import styled from "styled-components";
+import { ProductConsumer } from "../services/context";
 
 const Navbar = () => {
   return (
@@ -18,10 +19,13 @@ const Navbar = () => {
       </ul>
       <Link to="/cart" className="ml-auto">
         <Button>
-          <span className="mr-2">
+          <span className="">
             <i className="fa fa-shopping-cart" aria-hidden="true" />
+            <ProductConsumer>
+              {({ cart }) => <span className="cart-count">{cart.length}</span>}
+            </ProductConsumer>
+            <sub>Cart</sub>
           </span>
-          Cart
         </Button>
       </Link>
     </NavContainer>
@@ -34,10 +38,21 @@ const NavContainer = styled.nav`
     font-weight: 700;
     font-size: 1.7rem;
   }
+
   .nav-link {
     color: var(--nearWhite) !important;
     text-transform: capitalize;
     font-size: 1.3rem;
+  }
+
+  .cart-count {
+    font-size: 1rem;
+    padding: 4px 8px;
+    border-radius: 50%;
+    color: white;
+    background: red;
+    vertical-align: top;
+    margin-left: -5px;
   }
 `;
 
