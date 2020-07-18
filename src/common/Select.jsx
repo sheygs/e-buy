6cf92c192a17f5210cfb0f5a469247459bf7bc4e
sheygs13 <segun.ekoh@gmail.com>
2020-07-18 15:48:09 +0100

    feat: item size select option

--- src/common/Select.jsx
+++ src/common/Select.jsx
@@ -1,20 +1,20 @@
-import React from "react";
+import React from 'react';
 
 const Select = ({ size }) => {
   return (
     <h4 className="text-blue my-3">
       <strong>
         <span>sizes: </span>
-        {typeof size === "string" ? (
+        {typeof size === 'string' ? (
           <select
             style={{
-              maxWidth: "50%",
-              fontSize: "1rem",
+              maxWidth: '50%',
+              fontSize: '1rem',
             }}
             className="custom-select custom-select-sm mb-2"
           >
             <option value="">--select--</option>
-            {size.split(",").map((el, i) => (
+            {size.split(',').map((el, i) => (
               <option key={i} value={el}>
                 {el}
               </option>
