6b88644bc1b728216e690333147bcfe8dd2b5b9c
sheygs13 <segun.ekoh@gmail.com>
2020-09-21 21:46:23 +0100

    chore(refactor): clean-up codebase

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,10 +1,8 @@
-function formatPrice(price) {
-  // adds comma(s) to number
-  return `${price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`;
-}
+// adds comma(s) to number
+const formatPrice = (price) =>
+  `${price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`;
 
-const truncate = (words, limit = 12) => {
-  return words.length > limit ? `${words.slice(0, limit)}...` : words;
-};
+const truncate = (words, limit = 12) =>
+  words.length > limit ? `${words.slice(0, limit)}...` : words;
 
 export { truncate, formatPrice };
